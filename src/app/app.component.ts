b8f44a5881762e4e6931aa143ff7b78a5c200eb2
Juliana Da Silva Gomes <juliana.gomes@portaldedocumentos.com.br>
2021-09-17 10:51:28 -0300

    Exemplos de diretiva *ngIf e *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'exp-root',
@@ -6,8 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   //Criando as propriedades que serão um student
   luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'}
   leia = {name: 'Leia', isJedi: false}
   han = {name: 'Han Solo', isJedi: false}
+
+  //Criando um array de students
+  students: Student[] = [
+    {name: 'Estela', isJedi: true, temple: 'Kira'},
+    {name: 'Luís', isJedi: false},
+    {name: 'Hugo', isJedi: true, temple: 'Bronks'}
+  ];
 }
+
